240ed11bd6ec2d6f18d13a194f7884cafc48b07b
P-MH <miraculum95@gmail.com>
2023-10-19 20:36:41 +0900

    feat: github action

--- products/urls.py
+++ products/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from products.views import *
 
 urlpatterns = [
-    path('', MainView.as_view(), name='search'),
+    path('', MainView.as_view()),
     path('/search', ProductView.as_view(), name='search'),
 ]
